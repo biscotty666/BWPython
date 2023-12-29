d3d559173017630afc8a95bee6330f1d63f8480b
Brian Carey <biscotty666@gmail.com>
2023-12-28 23:45:54 -0700

    components

--- a_posts/views.py
+++ a_posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
 def home_view(request):
-    title = "Welcome to Django"
-    return render(request, "index.html", {'title': title})
+    print(request)
+    print(request.method)
+    return render(request, "a_posts/home.html")
