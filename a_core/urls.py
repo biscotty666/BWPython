cf19e0202a8939501524950a0e3f1d39804783eb
Brian Carey <biscotty666@gmail.com>
2023-12-31 22:10:46 -0700

    likes html

--- a_core/urls.py
+++ a_core/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('post/delete/<pk>/', post_delete_view, name='post-delete'),
     path('post/edit/<pk>/', post_edit_view, name='post-edit'),
     path('post/<pk>/', post_page_view, name='post'),
+    path('post/<pk>/like/', like_post, name='like-post'),
     path('category/<tag>/', home_view, name='category'),
     path('profile/', profile_view, name='profile'),
     path('<username>/', profile_view, name='userprofile'),
--- a_posts/views.py
+++ a_posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from .models import *
 from .forms import *
 from bs4 import BeautifulSoup
@@ -154,3 +155,15 @@ def reply_delete_view(request, pk):
         return redirect('post', reply.parent_comment.parent_post.id)
     
     return render(request, 'a_posts/reply_delete.html', {'reply': reply})
+
+def like_post(request, pk):
+    post = get_object_or_404(Post, id=pk)
+    user_exists = post.likes.filter(username=request.user.username).exists()
+    
+    if post.author != request.user:
+        if user_exists:
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+        
+    return render(request, 'snippets/likes.html', {'post': post})
