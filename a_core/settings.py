ee0de57842639ad882c46c68c4f8a097088bb6f4
Brian Carey <biscotty666@gmail.com>
2024-01-01 07:55:16 -0700

    cool htmx

--- a_core/settings.py
+++ a_core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django_htmx',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -54,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_htmx.middleware.HtmxMiddleware',
     "allauth.account.middleware.AccountMiddleware",
 ]
 
