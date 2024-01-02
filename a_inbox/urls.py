c724508c3b03a729d625e3f1b840f723ecf2c3d9
Brian Carey <biscotty666@gmail.com>
2024-01-02 09:19:16 -0700

    messages.1

--- a_inbox/urls.py
+++ a_inbox/urls.py
@@ -4,5 +4,7 @@ from .views import *
 urlpatterns = [
     path('', inbox_view, name='inbox'),
     path('c/<conversation_id>/', inbox_view, name="inbox"),
+    path('search_users/', search_users, name='inbox-searchusers'),
+    path('new_message/<recipient_id>/', new_message, name='inbox-newmessage'),
 ]
 
--- a_inbox/views.py
+++ a_inbox/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, Http404
+from django.db.models import Q
+from a_users.models import Profile
 from .models import *
+from .forms import InboxNewMessageForm
 
 @login_required
 def inbox_view(request, conversation_id=None):
@@ -14,3 +18,33 @@ def inbox_view(request, conversation_id=None):
         'my_conversations': my_conversations,
     }
     return render(request, 'a_inbox/inbox.html', context)
+
+def search_users(request):
+    letters = request.GET.get('search_user')
+    if request.htmx:
+        if len(letters) > 0:
+            profiles = Profile.objects.filter(realname__icontains=letters).exclude(realname=request.user.profile.realname)
+            # 'user' field contains the user id
+            # values_list returns a list of tuples, flat=True converts to a simple list
+            users_id = profiles.values_list('user', flat=True)
+            users = User.objects.filter(
+                Q(username__icontains=letters) | Q(id__in=users_id)
+            ).exclude(username=request.user.username)
+            return render(request, 'a_inbox/list_searchuser.html', { 'users': users })
+        else:
+            return HttpResponse('')
+    else:
+        raise Http404()
+
+@login_required    
+def new_message(request, recipient_id):
+    recipient = get_object_or_404(User, id=recipient_id)
+    new_message_form = InboxNewMessageForm()
+    context = {
+        'recipient': recipient,
+        'new_message_form': new_message_form
+    }
+    
+    return render(request, 'a_inbox/form_newmessage.html', context)
+    
+    
